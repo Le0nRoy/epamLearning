290cd9bbc4e97037dae5f222340ead57b151b134
le0n <cerber9283@gmail,com>
2019-11-25 04:49:05 +0300

    Add files to repository

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Main
+ *
+ * Version information 1.0
+ *
+ * Date 25.11.2019
+ *
+ * Copyright notice
+ */
+
+import tests.ArraysTest;
+import tests.StringsTest;
+
+public class Main {
+    static public void main( String[] args ){
+
+    }
+}
---
+++ src/MyStrings.java
@@ -0,0 +1,53 @@
+public class MyStrings {
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
+    public String makeOutWord(String out, String word) {
+        int half = out.length ( ) / 2;
+        return out.substring ( 0, half ) + word + out.substring ( half, out.length ( ) );
+    }
+
+    public String extraEnd(String str) {
+        String res = str.substring ( str.length ( ) - 2, str.length ( ) );
+        return res + res + res;
+    }
+
+    public String firstTwo(String str) {
+        if ( str.length ( ) < 2 ) {
+            return str;
+        }
+        String res = str.substring ( 0, 2 );
+        return res;
+    }
+
+    public String firstHalf(String str) {
+        return str.substring ( 0, str.length ( ) / 2 );
+    }
+
+    public String withoutEnd(String str) {
+        if ( str.length ( ) < 2 ) {
+            return "";
+        }
+        return str.substring ( 1, str.length ( ) - 1 );
+    }
+
+    public String comboString(String a, String b) {
+        if ( b.length ( ) > a.length ( ) ) {
+            return a + b + a;
+        }
+        return b + a + b;
+    }
+
+    public String nonStart(String a, String b) {
+        return a.substring ( 1, a.length ( ) ) + b.substring ( 1, b.length ( ) );
+    }
+}
